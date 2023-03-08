a238419f8c1aa949aedcdaf605d527d941cf790e
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-03-08 19:13:08 +0000

    big changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Hierophant</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style>
+    @import url('https://fonts.cdnfonts.com/css/tiro-devanagari-marathi');    
+    h1 {
+        font-family: 'Tiro Devanagari Marathi', sans-serif;
+        font-weight: 700;
+        font-size: xx-large;
+    }
+</style>
