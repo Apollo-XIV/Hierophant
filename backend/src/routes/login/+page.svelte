27efb47bdba9c0a8ce5f0302f7303c3fd4fdf7de
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-04-11 10:00:51 +0100

    some changes, some same

--- backend/src/routes/login/+page.svelte
+++ backend/src/routes/login/+page.svelte
@@ -1,6 +1,5 @@
 <script>
-    import { signIn, signOut } from '@auth/sveltekit/client';
-    import { page } from '$app/stores';
+    import { signIn } from '@auth/sveltekit/client';
 </script>
 
 <svelte:head>
@@ -24,9 +23,6 @@
             </filter></defs><g><rect width="100%" height="100%" fill="hsl(272, 100%, 50%)"></rect><rect width="100%" height="100%" fill="url(#gggrain-gradient3)"></rect><rect width="100%" height="100%" fill="url(#gggrain-gradient2)"></rect><rect width="100%" height="100%" fill="transparent" filter="url(#gggrain-filter)" opacity="0.45" style="mix-blend-mode: soft-light"></rect></g></svg>    </div>
     <div id="page">
         <h1>Hierophant Admin</h1>
-        <button on:click={() => signIn('github')}><!--<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-        </svg>--> Sign in with GitHub</button>    
         <p>This is the admin panel for Hierophant</p>
         <p>Please login via github to continue</p>
         <a on:click={() => signIn('github')} class="btn btn-block btn-social btn-github">
