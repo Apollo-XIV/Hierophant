713a2a945ea2916e25a6c7a00e900e880b61db42
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-05-02 15:28:23 +0100

    Update reup.sh

--- deploymentPackage/reup.sh
+++ deploymentPackage/reup.sh
@@ -2,4 +2,4 @@ sudo systemctl start docker
 sudo docker pull apolloxiv/uitest
 sudo docker kill proc123
 sudo docker rm proc123
-sudo docker run -dp 80:5173 --name proc123 apolloxiv/uitest
+sudo docker run -p 80:5173 --name proc123 apolloxiv/uitest
