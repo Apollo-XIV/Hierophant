d3626476d74d9206d6671145f8c0050a1fa51396
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-05-03 15:56:55 +0100

    ert

---
+++ frontend/src/lib/components/postcard.svelte
@@ -0,0 +1,156 @@
+<!-- // POSTCARD COMPONENT                                                                                                                                                   -->
+<!-- // USAGE:     <PostCard on:hover={hoverTest} on:leave={hoverTest} post={{title: "Default Title thats quite long", abstract: "test", author: "test", tags: ["test"]}} /> -->
+<!-- // OBJ FORMAT: {title: "", abstract: "", author: "", tags:[""]} -->
+
+<script>
+    import { goto } from '$app/navigation';
+    import { createEventDispatcher, onMount } from 'svelte';
+    import VanillaTilt from 'vanilla-tilt';
+    export let post = {
+        title: "test",
+        abstract: "test",
+        author: "test",
+        tags: ["#test", "#test"],
+    }
+    const dispatch = createEventDispatcher();
+    let component;
+    let rotation;
+
+    function hover() {
+        dispatch('hover', {target: component});
+    }
+
+    function leave() {
+        dispatch('leave');
+    }
+
+    onMount(() => {VanillaTilt.init(component, {max: 1, gyroscope: false, speed: 600, perspective: 1000,})})
+
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<component bind:this={component} on:focus={hover} on:mouseover={hover} on:mouseleave={leave} on:focusout={leave} tabindex="0" id="frame" on:keydown={(e) => {if(e.key == 'Enter') {goto('/posts')}}}  on:click={() => goto('/posts')} >
+    <glow-effect data-value={rotation}/>
+    <gradient-border />
+    <h3 id="post-title">{post.title}</h3>
+    <p id="post-abstract">{post.abstract}</p>
+    <p id="post-author" class="meta">{post.author}</p>
+    <divider/>
+    {#each post.tags as tag}
+    <p id="post-tags" class="meta">{tag}</p>
+    {/each}
+</component>
+
+<style>
+    #frame {
+        position: relative;
+        background-color: #11112436;
+        backdrop-filter: blur(5px);
+        border-radius: 12px;
+        box-sizing: border-box;
+        padding: 20px;
+        display: inline-flex;
+        flex-flow: row wrap;
+        align-items: stretch;
+        max-width: 800px;
+        min-width: fit-content;
+        width: 100%;
+        flex-basis: 800px;
+        gap: 5px;
+    }
+
+    #post-title {
+        display: block;
+        width: 100%;
+        font-family: 'IBM Plex Sans';
+        font-variant: small-caps;
+        font-weight: bold;
+        font-size: xx-large;
+        color: white;
+        line-height: 30px;
+        margin: 0;
+
+    }
+    
+    #post-abstract {
+        display: block;
+        width: 100%;
+        font-family: 'IBM Plex Mono';
+        font-size: large;
+        font-style: italic;
+        color: white;
+        margin: 0;
+    }
+
+    #post-author {
+        display: inline;
+    }
+
+    #post-tags {
+        display: content;
+    }
+
+    divider {
+        flex-grow: 1;
+    }
+
+    .meta {
+        margin: 0;
+        color: white;
+        font-family: 'IBM Plex Sans';
+        font-weight: lighter;
+        font-size: larger;
+        text-transform: uppercase;
+    }
+
+    gradient-border {
+        position: absolute;
+        inset: 0;
+        padding: 2px;
+        border-radius: 12px;
+        background: linear-gradient(336deg, rgba(255,226,161,1) 0%, rgba(231,114,255,1) 54%, rgba(0,212,255,1) 100%);
+        -webkit-mask:
+            linear-gradient(#fff 0 0) content-box, 
+            linear-gradient(#fff 0 0);
+                mask: 
+            linear-gradient(#fff 0 0) content-box, 
+            linear-gradient(#fff 0 0);
+        -webkit-mask-composite: xor;
+                mask-composite: exclude;
+    }
+
+    glow-effect {
+        pointer-events: none;
+        opacity: 1;
+        position: absolute;
+        inset: -150px;
+        border: 150px solid #0000;
+        border-radius: 165px;
+        transform: translateZ(-1px);
+        -webkit-mask:
+            linear-gradient(#000 0 0) content-box,
+            linear-gradient(#000 0 0);
+        mask:
+            linear-gradient(#000 0 0) content-box,
+            linear-gradient(#000 0 0);
+        -webkit-mask-composite: xor;
+                mask-composite: exclude;
+    }
+
+    glow-effect::before {
+        content: "";
+        position: absolute;
+        inset: -5px;
+        border-radius: 12px;
+        background: linear-gradient(336deg, rgba(255,226,161,1) 0%, rgba(231,114,255,1) 54%, rgba(0,212,255,1) 100%);
+        filter: blur(10px);
+        opacity: 0;
+        transition: opacity ease-out 0.5s;
+        pointer-events: none;
+    }
+
+    #frame:hover glow-effect::before {
+        opacity: 1;
+    }
+
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,101 +1,68 @@
 <svelte:head>
 	<title>About | Hierophant</title> 
+    <link rel="preload" as="style" href="https://fonts.cdnfonts.com/css/ibm-plex-sans" crossorigin>
+    <link href="https://fonts.cdnfonts.com/css/ibm-plex-sans" rel="stylesheet" crossorigin>
+    <link rel="preload" as="style" href="https://fonts.cdnfonts.com/css/ibm-plex-mono-3" crossorigin>
+    <link href="https://fonts.cdnfonts.com/css/ibm-plex-mono-3" rel="stylesheet" crossorigin>
+    <link rel="preload" as="style" href="https://fonts.cdnfonts.com/css/grozery" crossorigin>
+    <link href="https://fonts.cdnfonts.com/css/grozery" rel="stylesheet" crossorigin>
 </svelte:head>
 
-<div id="main">
-    <div id="logo"><h1>Hierophant</h1></div>
-    <div id="copy">
-        <h2>About Hierophant</h2>
-        <p>Welcome to Hierophant, my personal blog where I explore the world of DevOps and technology as a learning apprentice. Here, I share my personal experiences, insights, and projects related to the latest trends and practices in the field.</p>
-        <p><a href="/posts">Check out posts -></a></p>
-        <h3>Who am I</h3>
-        <p>I am a passionate technology enthusiast with a keen interest in DevOps and automation. I am currently an apprentice who loves to tinker with technology and explore new tools and practices. Sharing the things I know and making the field more accessible is incredibly important for me, so as I learn more things and develop my personal skills, I plan to post here and share any tips or tricks I found along the way.</p>
-        <h3>What I do</h3>
-        <p>With Hierophant, I share my personal experiences and projects related to DevOps and technology. I cover a wide range of topics, including:</p>
-        <ul>
-            <li>Automation and scripting</li>
-            <li>Containerization and virtualization</li>
-            <li>Cloud computing and serverless architecture</li>
-            <li>Infrastructure as code (IaC)</li>
-            <li>CI/CD pipelines and tools</li>
-            <li>Monitoring and logging</li>
-        </ul>     
-        <p>My goal is to share my personal experiences and insights with other learners and hobbyists who are also passionate about exploring the latest trends and practices in the field.</p>
-        <h3>Why I do it</h3>
-        <p>I believe that technology is a powerful tool that can be used to solve complex problems and make the world a better place. As a hobbyist, I enjoy exploring new tools and practices and sharing my experiences with others.</p>
-        <p>Thank you for visiting Hierophant, and I hope you find my content informative and useful. </p>
+
+<script>
+    import PostCard from '$lib/components/postcard.svelte';
+    import { onMount } from 'svelte';
+
+</script>
+
+<div class="parallax">
+    <div class="parallax-layer" id="background"></div>
+
+
+    <div class="parallax-layer" id="middleground"></div>
+
+
+    <div class="parallax-layer" id="foreground">
+        <p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p>
     </div>
-    <div id="buffer" />
 </div>
 
+
+
 <style>
-    @import url('https://fonts.cdnfonts.com/css/futura-std-4');
-    @import url('https://fonts.cdnfonts.com/css/ibm-plex-sans-kr');
-    @import url('https://fonts.cdnfonts.com/css/monosphere-personal-use');
-    div#main {
-        background-color: black;
-        background-image: url('/bgimage.png');
-        background-repeat: no-repeat;
-        background-size: 40vw;
-    }
-    
-    div#copy {
-        max-width: 1200px;
-        width: auto;
-        margin: auto;
-        padding-left: 20px;
-        padding-right: 20px;
+    .parallax {
+        height: 100vh;
+
+        overflow-x: hidden;
+        overflow-y: auto;
+
+        perspective: 1px;
     }
 
-    div#logo {
-        background-image: url('/Logofaint.png');
-        background-size: 400px;
-        background-repeat: no-repeat;
-        background-position: center;
-        padding: 50px;
-        width: fit-content;
-        height: 300px;
-        align-self: center;
-        margin: auto;
-        padding-top: 100px;
-        
-    }
-
-    h1, h2, h3, p, li {
-        color: white;
-        box-shadow: 5px 5px 86px 20px #000;
-        background-color: #00000077;
-    }
-
-    h1 {
-        font-family: 'Monosphere Personal Use';
-        text-align: center;
-        /*background-color: rgb(219, 175, 30);*/
-        height: 100%;
-        background-color: #0000008f;
-        line-height: 300px;
-        
-    }
-
-    h2, h3 {
-        font-family: 'IBM Plex Sans Kr';
-    }
-    p, a {
-        font-family: 'Futura Std';
-        color: white;
-    }
-
-    ul {
-        list-style-type: none;
-        font-family: 'Futura Std';
-    }
-
-    ul li:before {
-        content: '\2014';
+    .parallax-layer {
         position: absolute;
-        margin-left: -20px;
+        inset: 0;
     }
-    div#buffer {
-        height: 100px;
+
+    #background {
+        transform: translateZ(-2px);
+        top:50px;
+        background: red;
     }
+
+    #middleground {
+        transform: translateZ(-1px);
+        top: 60px;
+        background-color: green;
+    }
+
+    #foreground {
+        transform: translateZ(0px);
+        top: 60px
+    }
+
+    :global(body) {
+        margin: 0;
+    }
+
 </style>
