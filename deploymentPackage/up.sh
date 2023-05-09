30b3c9d702292332a21fddd4249b686e6c78a210
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2023-05-09 12:47:46 +0100

    Update up.sh

--- deploymentPackage/up.sh
+++ deploymentPackage/up.sh
@@ -4,5 +4,5 @@ sudo docker compose pull
 sudo docker stop $(docker ps -a -q)
 sudo docker rm $(docker ps -a -q)
 docker run -d --name newrelic-infra --network=host --cap-add=SYS_PTRACE --privileged --pid=host -v "/:/host:ro" -v "/var/run/docker.sock:/var/run/docker.sock" -e  NRIA_LICENSE_KEY=eu01xx51db3f05d448877ca65cc1004072ebNRAL newrelic/infrastructure:latest
-sudo docker compose up 
+sudo docker compose up
 sudo docker system prune --force
